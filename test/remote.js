1c7292ea0416e0cbafda49bcea13773df197042d
ajlopez <webmaster@ajlopez.com>
2012-06-30 10:48:32 -0300

    New test getting null message from remote empty queue

--- test/remote.js
+++ test/remote.js
@@ -54,6 +54,40 @@ exports['Get Message']= function(test) {
     client.connect(3000);
 }
 
+exports['Get Message and Null']= function(test) {
+    test.expect(7);
+    var server = new QueueServer();    
+    var queue = server.createQueue('queue');
+    queue.putMessage(new Message('foo'));
+    
+    var remoteserver = simplequeue.createRemoteServer(server);
+    remoteserver.listen(3000);
+    
+    var client = simplequeue.createRemoteClient();
+    
+    client.on('remote', function(remote) {
+        remote.getQueue('queue', function(err, queue) {
+            test.equal(err, undefined);
+            test.ok(queue);
+            
+            queue.getMessage(function (err, msg) {
+                test.equal(err, undefined);
+                test.ok(msg);
+                test.equal(msg.payload, 'foo');
+                queue.getMessage(function (err, msg) {
+                    test.equal(err, undefined);
+                    test.equal(msg, null);
+                    client.end();
+                    remoteserver.close();
+                    test.done();
+                });
+            });
+        });
+    });
+    
+    client.connect(3000);
+}
+
 exports['Get null Message from empty Queue']= function(test) {
     test.expect(4);
     var server = new QueueServer();    
@@ -75,6 +109,38 @@ exports['Get null Message from empty Queue']= function(test) {
                 client.end();
                 remoteserver.close();
                 test.done();
+            });
+        });
+    });
+    
+    client.connect(3000);
+}
+
+exports['Get two null Messages from empty Queue']= function(test) {
+    test.expect(6);
+    var server = new QueueServer();    
+    var queue = server.createQueue('queue');
+    
+    var remoteserver = simplequeue.createRemoteServer(server);
+    remoteserver.listen(3000);
+    
+    var client = simplequeue.createRemoteClient();
+    
+    client.on('remote', function(remote) {
+        remote.getQueue('queue', function(err, queue) {
+            test.equal(err, undefined);
+            test.ok(queue);
+            
+            queue.getMessage(function (err, msg) {
+                test.equal(err, undefined);
+                test.equal(msg, null);
+                queue.getMessage(function (err, msg) {
+                    test.equal(err, undefined);
+                    test.equal(msg, null);
+                    client.end();
+                    remoteserver.close();
+                    test.done();
+                });
             });
         });
     });
