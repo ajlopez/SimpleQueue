2687b3dd5214ab315864e647ab150fd4cd2f0bb2
ajlopez <webmaster@ajlopez.com>
2012-07-01 18:44:18 -0300

    Remote consume with test

--- test/remote.js
+++ test/remote.js
@@ -88,6 +88,54 @@ exports['Get Message and Null']= function(test) {
     client.connect(3000);
 }
 
+exports['Remote Consume']= function(test) {
+    test.expect(9);
+    var server = new QueueServer();    
+    var queue = server.createQueue('queue');
+    queue.putMessage(new Message(1));
+    queue.putMessage(new Message(2));
+    queue.putMessage(new Message(3));
+    
+    var remoteserver = simplequeue.createRemoteServer(server);
+    remoteserver.listen(3000);
+    
+    var client = simplequeue.createRemoteClient();
+    
+    client.on('remote', function(remote) {
+        remote.getQueue('queue', function(err, queue) {
+            test.equal(err, undefined);
+            test.ok(queue);
+            
+            var sum = 0;
+            
+            simplequeue.remoteConsume(queue, function(err, msg) {
+                if (err) {
+                    console.log(err);
+                    return false;
+                }
+                
+                test.ok(msg);
+                test.ok(msg.payload);
+                
+                sum += msg.payload;
+                
+                if (msg.payload != 3)
+                    return true;
+                    
+                test.equal(sum, 6);
+                    
+                client.end();
+                remoteserver.close();
+                test.done();
+                
+                return false;
+            });
+        });
+    });
+    
+    client.connect(3000);
+}
+
 exports['Get null Message from empty Queue']= function(test) {
     test.expect(4);
     var server = new QueueServer();    
